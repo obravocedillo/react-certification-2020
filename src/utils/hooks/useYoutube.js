98e94ef61f1f8709ca115a63dfcf9c380d97d1a9
Oliver Bravo <oliverbravo45@gmail.com>
2021-04-13 15:43:38 -0500

    fix: leak of api key

--- src/utils/hooks/useYoutube.js
+++ src/utils/hooks/useYoutube.js
@@ -8,7 +8,6 @@ function useYoutube(searchTerm = 'wizeline') {
 
   async function searchVideos(newSearch) {
     try {
-      console.log(process.env.REACT_APP_YOUTUBE_KEY);
       const startingUrlResult = await axios.get`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=12&q=${newSearch}&type=video&key=${process.env.REACT_APP_YOUTUBE_KEY}`();
       dispatch({
         type: 'CHANGE_VIDEOS',
